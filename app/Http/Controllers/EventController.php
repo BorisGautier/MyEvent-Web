26c301f2f2432ac0e6158c10274757478c3c4172
BorisGautier <tchoukouahaboris@outlook.fr>
2021-04-10 10:29:08 +0100

    eventController update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -84,7 +84,7 @@ class EventController extends BaseController
             if ($request->file('cover')->isValid()) {
                 $extension = $request->cover->extension();
                 $path1      = $request->cover->storeAs($user->name . '/' . $request->codeEvent . '/cover', 'cover.' . $extension, 'public');
-                $url       = env("APP_URL") . '/' . $user->name . '/' . $request->codeEvent . '/cover/' . 'cover.' . $extension;
+                $url       = storage_path() . $user->name . '/' . $request->codeEvent . '/cover/' . 'cover.' . $extension;
             }
 
 
